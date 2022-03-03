a08e7f95b34e6577d72496c8588112f9cade151c
Volodymyr SamoilenkoojtuQDM[L <vssamoilenko@gmail.com>
2022-03-03 22:31:53 +0200

    Add file testgit.txt1

--- src/main/java/com/foxminded/charcounter/Main.java
+++ src/main/java/com/foxminded/charcounter/Main.java
@@ -3,7 +3,7 @@ package com.foxminded.charcounter;
 public class Main {
 
     public static void main(String[] args) {
-        //Add change for Git 122
+        //Add change for Git 1223
         CharCounter createTable = new CharCounter();
         String getInputText = "";
         while (!getInputText.equals("q")) {
