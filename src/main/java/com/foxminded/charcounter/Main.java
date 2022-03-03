47272b596949ebb26e5ee01a94d73b08565ca20a
Volodymyr Samoilenko <vssamoilenko@gmail.com>
2022-03-03 17:45:37 +0200

    Add comments in Main class

--- src/main/java/com/foxminded/charcounter/Main.java
+++ src/main/java/com/foxminded/charcounter/Main.java
@@ -3,7 +3,7 @@ package com.foxminded.charcounter;
 public class Main {
 
     public static void main(String[] args) {
-
+        //Add change for Git
         CharCounter createTable = new CharCounter();
         String getInputText = "";
         while (!getInputText.equals("q")) {
