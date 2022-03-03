f435c38fdadfa44c021c57991ba99be9322044b1
vssamoilenko <vssamoilenko@gmail.com>
2022-03-03 21:20:31 +0200

    Update Main.java

--- src/main/java/com/foxminded/charcounter/Main.java
+++ src/main/java/com/foxminded/charcounter/Main.java
@@ -3,7 +3,7 @@ package com.foxminded.charcounter;
 public class Main {
 
     public static void main(String[] args) {
-        //Add change for Git 1
+        //Add change for Git 12
         CharCounter createTable = new CharCounter();
         String getInputText = "";
         while (!getInputText.equals("q")) {
