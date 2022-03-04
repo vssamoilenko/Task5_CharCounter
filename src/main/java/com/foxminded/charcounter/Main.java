e211dd4cee5a1a176d5d5190d08bcf129ddfcd52
Volodymyr SamoilenkoojtuQDM[L <vssamoilenko@gmail.com>
2022-03-04 20:40:06 +0200

    5 main

--- src/main/java/com/foxminded/charcounter/Main.java
+++ src/main/java/com/foxminded/charcounter/Main.java
@@ -3,7 +3,7 @@ package com.foxminded.charcounter;
 public class Main {
 
     public static void main(String[] args) {
-        //4
+        //5
         CharCounter createTable = new CharCounter();
         String getInputText = "";
         while (!getInputText.equals("q")) {
