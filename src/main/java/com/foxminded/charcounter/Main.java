1a7cee493b61049db6f63abfbe7cc2b0be5a7d6a
Volodymyr SamoilenkoojtuQDM[L <vssamoilenko@gmail.com>
2022-03-04 19:29:04 +0200

    add chamge for git after checkout

--- src/main/java/com/foxminded/charcounter/Main.java
+++ src/main/java/com/foxminded/charcounter/Main.java
@@ -3,7 +3,7 @@ package com.foxminded.charcounter;
 public class Main {
 
     public static void main(String[] args) {
-        //127
+        //1278
         CharCounter createTable = new CharCounter();
         String getInputText = "";
         while (!getInputText.equals("q")) {
